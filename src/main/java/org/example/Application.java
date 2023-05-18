9dd6577907033914807b0a6dbbf7803ae48575c4
Papaofarchi <korenevdenismain@gmail.com>
2023-05-18 22:46:42 +0300

    new

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,13 +1,37 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
+    private static Connection con;
+    private static Statement stmt;
+    private static final String url = "jdbc:mysql://localhost:3306/shem";
+    private static final String username = "root";
+    private static final String password = "root";
+
+
+
+    public static void main(String[] args) throws SQLException {
+
+
+
+
+        System.out.println("Connecting database...");
+        con = DriverManager.getConnection(url, username, password);
+        stmt = con.createStatement();
+
+
         Scanner in = new Scanner(System.in);
-        System.out.print(" Enter name and surname \n");
+        System.out.print(" Enter name, surname and email \n");
         String name = in.nextLine();
         String surname = in.nextLine();
-        System.out.printf(" Name is " + name, "and surname is" + surname);
+        String email = in.nextLine();
+        String query = "INSERT INTO shem.persons (name, surname, email) \n" +
+                " VALUES ('" + name + "' , '" + surname + "' , '" +  email+ "');";
+        stmt.executeUpdate(query);
     }
 }
