a672650fed77d6a19e9b674b707c36f6cb4e0a9a
Papaofarchi <korenevdenismain@gmail.com>
2023-05-19 00:14:18 +0300

    update

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -5,6 +5,7 @@ import org.example.service.ConsoleService;
 public class Application {
     public static void main(String[] args) {
         ConsoleService consoleService = new ConsoleService();
+
         consoleService.printInput("Enter name and surname ");
         String name = consoleService.getInput();
         String surname = consoleService.getInput();
