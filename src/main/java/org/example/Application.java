38f040a4b27cfe1f18c0bc3e719a5182d0bc45fa
Papaofarchi <korenevdenismain@gmail.com>
2023-05-19 00:19:43 +0300

    update

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import org.example.service.ConsoleService;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class Application {
 
@@ -18,14 +19,15 @@ public class Application {
 
     public static void main(String[] args) throws SQLException {
         Application x = new Application();
+        ConsoleService consoleService= new ConsoleService();
         System.out.println("Connecting database...");
         x.con = DriverManager.getConnection(x.url, x.username, x.password);
         x.stmt = x.con.createStatement();
-        Scanner in = new Scanner(System.in);
+
         System.out.print(" Enter name, surname and email \n");
-        String name = in.nextLine();
-        String surname = in.nextLine();
-        String email = in.nextLine();
+        String name = consoleService.getInput();
+        String surname = consoleService.getInput();
+        String email = consoleService.getInput();
         String query = "INSERT INTO shem.persons (name, surname, email) \n" +
                 " VALUES ('" + name + "' , '" + surname + "' , '" +  email+ "');";
         x.stmt.executeUpdate(query);
